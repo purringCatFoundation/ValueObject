582c3112f05d500af1642a00e604eedb43ba1804
adeptofvoltron <b.vanderesch@gmail.com>
2017-03-12 14:31:07 +0100

    corrections

--- src/ValueObjectInterface.php
+++ src/ValueObjectInterface.php
@@ -9,13 +9,16 @@ interface ValueObjectInterface
     /**
      * Two value objects are equal if their values are equal
      * @param ValueObjectInterface $compare
+     *
      * @return boolean
      */
     public function isEqualTo(ValueObjectInterface $compare): bool ;
 
     /**
      * Return string representation of value
+     *
      * @return string
      */
     public function __toString(): string;
 }
+
