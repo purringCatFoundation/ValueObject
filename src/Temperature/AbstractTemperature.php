d392a62e765fe6aad8c1a2470a1ca17eba517a0d
pradzikowski <paw.radzikowski@gmail.com>
2017-03-26 13:24:50 +0200

    Introducing Temperature ValueObjects

---
+++ src/Temperature/AbstractTemperature.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCF\ValueObject\Temperature;
+
+use PCF\ValueObject\ValueObjectInterface;
+
+abstract class AbstractTemperature implements ValueObjectInterface
+{
+
+}
